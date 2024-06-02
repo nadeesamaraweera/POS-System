18c110063d44b019f9c61bb39f25de2ad2e7fb0c
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-06-02 19:51:24 +0530

    added the customerController.js

--- Controller/CustomerController.js
+++ Controller/CustomerController.js
@@ -1,4 +1,19 @@
-import {saveCustomer} from'../model/CustomerModel.js'
-import {getAllCustomer} from'../model/CustomerModel.js'
-import {updateCustomer} from'../model/CustomerModel.js'
-import {deleteCustomer} from'../model/CustomerModel.js'
+import {saveCustomer} from '../model/CustomerModel.js'
+import {getAllCustomer} from '../model/CustomerModel.js'
+import {updateCustomer} from '../model/CustomerModel.js'
+import {deleteCustomer} from  '../model/CustomerModel.js'
+
+
+$(document).ready(function(){
+    refresh();
+});
+
+document.querySelector('#CustomerManage #customerForm').addEventListener('submit' , function(event){
+    event.preventDefault();
+
+})
+
+var customerId ;
+var customerName;
+var customerAddress;
+var customerSalary;
