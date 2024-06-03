885d23a8c7e78299895cb5b5e85afc08c1952e50
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-06-03 22:04:33 +0530

    add the javascript in customer manage

--- Controller/CustomerController.js
+++ Controller/CustomerController.js
@@ -111,6 +111,122 @@ function loadTable(customer){
             '<td>' + customer.custSalary + '</td>' +
         '</tr>' 
     );
+    
 }
+
+function extractNumber(id) {
+    var match = id.match(/C0(\d+)/);
+    if (match && match.length > 1) {
+        return parseInt(match[1]);
+    }
+    return null;
+}
+
+function createCustomerId() {
+    let customers = getAllCustomers();
+    
+    if (!customers || customers.length === 0) {
+        return 'C01';
+    } else {
+        let lastCustomer = customers[customers.length - 1];
+        let id = lastCustomer && lastCustomer.custId ? lastCustomer.custId : 'C00';
+        
+        let number = extractNumber(id);
+        number++;
+        return 'C0' + number;
+    }
+}
+
+function refresh(){
+    $('#CustomerManage .custId').val(createCustomerId());
+    $('#CustomerManage .custName').val('');
+    $('#CustomerManage .custAddress').val('');
+    $('#CustomerManage .custSalary').val('');
+    $('#CustomerManage .invalidCustId').text('');
+    $('#CustomerManage .invalidCustName').text('');
+    $('#CustomerManage .invalidCustAddress').text('');
+
+    reloadTable();
+}
+
+$('#CustomerManage .cleatBtn').click(function(){
+    refresh();
+});
+
+$('#CustomerManage .searchBtn').click(function(){
+    let customer = searchCustomer($('#CustomerManage .custId').val());
+    if(customer){
+        $('#CustomerManage .custName').val(customer.custName);
+        $('#CustomerManage .custAddress').val(customer.custAddress);
+        $('#CustomerManage .custSalary').val(customer.custSalary);
+    }
+    else{
+        alert('Customer Not Found');
+    }
+});
+
+function searchCustomer(id){
+    let customers = getAllCustomers();
+    let customer = customers.find(c => c.custId === id);
+    return customer;
+}
+
+$('#CustomerManage .updateBtn').click(function(){
+    
+    let UpdateCustomer = {
+        custId : 'C00',
+        custName : $('#CustomerManage .custName').val(),
+        custAddress : $('#CustomerManage .custAddress').val(),
+        custSalary : $('#CustomerManage .custSalary').val()
+    }
+
+    let validResult = validate(UpdateCustomer);
+
+    UpdateCustomer.custId = $('#CustomerManage .custId').val();
+    
+    if(validResult){
+        let customers = getAllCustomers();
+        let index = customers.findIndex(c => c.custId === UpdateCustomer.custId);
+        updateCustomer(index, UpdateCustomer);
+        alert('Customer Updated');
+        refresh();
+    }
+
+});
+
+function reloadTable(){
+    let customers = getAllCustomers();
+    $('#CustomerManage .tableRow').empty();
+    customers.forEach(c => {
+        loadTable(c);
+    });
+}
+
+$('#CustomerManage .removeBtn').click(function(){
+    let customers = getAllCustomers();
+    let id = $('#CustomerManage .custId').val();
+
+    let index = customers.findIndex(c => c.custId === id);
+    if(index >= 0){
+        deleteCustomer(index);
+        alert('Customer Deleted');
+        refresh();
+    }
+    else{
+        alert('Customer Not Found');
+    }
+});
+
+$('#CustomerManage .tableRow').on('click', 'tr', function(){
+    let id = $(this).children('td:eq(0)').text();
+    let name = $(this).children('td:eq(1)').text();
+    let qty = $(this).children('td:eq(2)').text();
+    let price = $(this).children('td:eq(3)').text();
+
+    $('#CustomerManage .custId').val(id);
+    $('#CustomerManage .custName').val(name);
+    $('#CustomerManage .custAddress').val(qty);
+    $('#CustomerManage .custSalary').val(price);
+});
 
     
