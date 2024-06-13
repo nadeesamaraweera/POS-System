59ed76a3d8242e9dfb2bf9c5120e658074f203b5
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-06-13 11:03:33 +0530

    update

--- db/DB.js
+++ db/DB.js
@@ -3,14 +3,35 @@ export let Customers = [
         custId : 'C001',
         custName : 'Nadeesha',
         custAddress : 'Matara',
-        custSalary : '25'
+        custSalary : '25000.00'
     } ,
 
     {
         custId : 'C002',
         custName : 'Madushan',
         custAddress : 'Matara',
-        custSalary : '30'
+        custSalary : '50000.00'
+    },
+
+    {
+        custId : 'C003',
+        custName : 'Sandeepa',
+        custAddress : 'Colombo',
+        custSalary : '25000.00'
+    },
+
+    {
+        custId : 'C004',
+        custName : 'Randana',
+        custAddress : 'Galle',
+        custSalary : '30000.00'
+    },
+
+    {
+        custId : 'C005',
+        custName : 'Charith',
+        custAddress : 'Kandy',
+        custSalary : '50000.00'
     }
 ];
 export let Items = [
@@ -25,6 +46,24 @@ export let Items = [
         itemName : 'Book',
         itemQty : 10,
         itemPrice : 200
-    }
+    },
+    {
+        itemId : 'I003',
+        itemName : 'Shampo',
+        itemQty : 20,
+        itemPrice : 110
+    },
+    {
+        itemId : 'I004',
+        itemName : 'Pen',
+        itemQty : 100,
+        itemPrice : 25
+    },
+    {
+        itemId : 'I005',
+        itemName : 'Bag',
+        itemQty : 50,
+        itemPrice : 2000
+    },
 ];
 export let Orders = [];
